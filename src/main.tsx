2ceebda5002258a92f1814bec0d597113ddc8cde
Tadako0727 <kowita51@gmail.com>
2026-07-17 04:24:32 +0900

    Refresh mobile UI cache

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,5 @@ import App from './App';
 import './styles.css';
 import './next-shift.css';
 import './mobile-form.css';
-if('serviceWorker' in navigator) window.addEventListener('load',()=>void navigator.serviceWorker.register('/sw.js'));
+if('serviceWorker' in navigator) window.addEventListener('load',()=>void navigator.serviceWorker.register('/sw.js').then(registration=>registration.update()));
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
