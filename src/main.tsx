b0b84b95c276cd2b4c33d6bf6530ca079d7dc137
Tadako0727 <kowita51@gmail.com>
2026-08-06 01:59:15 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 import './styles.css';
 import './next-shift.css';
 import './mobile-form.css';
+import './avatar-crop.css';
 if('serviceWorker' in navigator) window.addEventListener('load',()=>void navigator.serviceWorker.register('/sw.js').then(registration=>registration.update()));
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
